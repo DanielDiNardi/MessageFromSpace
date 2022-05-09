dc84bbeee6ae5e1602be43b8d415deef984af884
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-05-09 10:25:47 +0100

    Finished scene 6

--- Assets/Scipts/MoveShipForward.cs
+++ Assets/Scipts/MoveShipForward.cs
@@ -6,18 +6,6 @@ public class MoveShipForward : MonoBehaviour
 {
     public float speed = 10f;
 
-    IEnumerator ActivateBoosters()
-    {
-        yield return new WaitForSeconds(5);
-        Debug.Log("Boosters Activated");
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine("ActivateBoosters");
-    }
-
     // Update is called once per frame
     void Update()
     {
